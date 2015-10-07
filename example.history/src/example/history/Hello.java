36067f21ffcb51d01cf07b1cdff921c6cd15abf2
Manu <manu.alem@gmail.com>
2015-10-07 16:49:54 +0200

    name field and set name method defined

--- example.history/src/example/history/Hello.java
+++ example.history/src/example/history/Hello.java
@@ -2,6 +2,11 @@ package example.history;
 
 public class Hello {
 	private int age;
+	private String name;
     public Hello(){}
     public Hello(String message){}
+    
+    public void setName(String name){
+    	this.name = name;
+    }
 }
