dc48276b0593166ce0ceaa2c99f1a14915c2e932
manualem <manu.alem@gmail.com>
2015-10-09 13:29:06 +0200

    Layout modified

--- example.history/src/example/history/Hello.java
+++ example.history/src/example/history/Hello.java
@@ -3,10 +3,11 @@ package example.history;
 public class Hello {
 	private int age;
 	private String name;
-    public Hello(){}
-    public Hello(String message){}
+        
+        public Hello(){}
+        public Hello(String message){}
     
-    public void setName(String name){
-    	this.name = name;
-    }
+        public void setName(String name){
+    	   this.name = name;
+        }
 }
